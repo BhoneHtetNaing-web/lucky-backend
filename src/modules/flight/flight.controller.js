0161b58bd6f22ace518635fbbceb0a7b6c983659
Bhone Htet Naing <naingbhonehtet70@gmail.com>
2026-04-18 14:18:43 +0630

    fix flight.controller

--- src/modules/flight/flight.controller.js
+++ src/modules/flight/flight.controller.js
@@ -1,35 +1,9 @@
-const { searchFlights }, service = require('./flight.service');
+const service = require('./flight.service');
 
-const searchFlights = async (req, res) => {
+exports.searchFlights = async (req, res) => {
     const { from, to, date } = req.query;
 
     const data = await service.searchFlights(from, to, date);
 
     res.json(data);
 };
-
-// const getFlights = async (req, res) => {
-//     try {
-//         const { from, to, date } = req.query;
-
-//         if (!from || !to || !date) {
-//             return res.status(400).json({
-//                 message: 'Missing parameters',
-//             });
-//         }
-
-//         const flights = await searchFlights({ from, to, date });
-
-//         res.json({
-//             success: true,
-//             data: flights,
-//         });
-//     } catch (err) {
-//         res.status(500).json({
-//             success: false,
-//             message: err.message,
-//         });
-//     }
-// };
-
-module.exports = { searchFlights };
