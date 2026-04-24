914745860e7676ba2b26b87179605db2d45f69db
Bhone Htet Naing <naingbhonehtet70@gmail.com>
2026-04-24 19:26:14 +0630

    Fix getStats

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -2,44 +2,38 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('./admin.controller');
-const admin = require('../../middleware/admin.middleware');
-const auth = require("../../middleware/auth.middleware");
+const { adminMiddleware } = require('../../middleware/admin.middleware');
+const authMiddleware = require("../../middleware/auth.middleware");
 
 // Protected all routes
 router.use(admin);
 
 // DASHBOARD
-router.get("/dashboard", auth, admin, controller.dashboard);
+router.get("/dashboard", authMiddleware, adminMiddleware, controller.getDashboard);
 //
-router.get('/stats', auth, async (req, res) => {
-    res.json({
-        users: 10,
-        bookings: 5,
-        payments: 3,
-    });
-});
+router.get('/stats', authMiddleware, controller.getStats);
 // Bookings
-router.get('/bookings', auth, async (req, res) => {
+router.get('/bookings', authMiddleware, async (req, res) => {
     // DB query here
     res.json([]);
 });
 // Payments
-router.get('/payments', auth, async (req, res) => {
+router.get('/payments', authMiddleware, async (req, res) => {
     res.json([]);
 });
 // Messages
-router.get('/messages', auth, async (req, res) => {
+router.get('/messages', authMiddleware, async (req, res) => {
     res.json([]);
 });
 // APPROVE
-router.post('/bookings/:id/approve', auth, controller.approveBooking);
+router.post('/bookings/:id/approve', authMiddleware, controller.approveBooking);
 // BookingById
-router.get('/bookings/:id', controller.getBookingById);
+router.get('/bookings/:id', authMiddleware, controller.getBookingById);
 // Fix BOOKING
-router.put('/bookings/:id/cancel', controller.cancelBooking);
+router.put('/bookings/:id/cancel',authMiddleware, controller.cancelBooking);
 // REJECT
-router.post('/reject/:id', auth, controller.reject);
+router.post('/reject/:id', authMiddleware, controller.reject);
 // APPROVE KBZPAY
-router.put('/kbz/approve/:id', auth, controller.approveKBZPayment);
+router.put('/kbz/approve/:id', authMiddleware, controller.approveKBZPayment);
 
 module.exports = router;
